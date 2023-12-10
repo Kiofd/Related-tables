da5c18e4c6cf6f57fd4e9c44cec6c02c4a712141
linecatdog@gmail.com <linecatdog@gmail.com>
2023-12-10 23:29:41 +0200

    update3

---
+++ Sprint16/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Sprint16.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Sprint16
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ShoppingContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            
+            services.AddMvc();
+            services.AddControllersWithViews();
+            //services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
