9575214d55fea02651df5f15bcad5656291ef1e0
Maksym <max.sheremeta228@gmail.com>
2023-12-11 17:43:28 +0200

    Create controllers

---
+++ Sprint16/Controllers/SupermarketsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sprint16.Controllers
+{
+    public class SupermarketsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
