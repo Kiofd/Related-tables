dc14433362b3d3835e96ab5255050a220fe3bcc6
linecatdog@gmail.com <linecatdog@gmail.com>
2023-12-11 15:00:16 +0200

    UpdateDataBase

--- Sprint16/Data/SampleData.cs
+++ Sprint16/Data/SampleData.cs
@@ -6,114 +6,55 @@ namespace Sprint16.Data
     {
         public static void Initialize(ShoppingContext context)
         {
-            if (context.Products.Any())
+            if (context.Products.Any() || context.Customers.Any() || context.Supermarkets.Any() || context.Orders.Any())
             {
-                return;
+                return; 
             }
-            context.Products.AddRange(
-                new Product
-                {
-                    Name = "Butter",
-                    Price = 30.0f
-                },
-                new Product
-                {
-                    Name = "Banana",
-                    Price = 20.50f
-                },
-                new Product
-                {
-                    Name = "Morshinska",
-                    Price = 9.30f
-                }
-            );
-            context.Customers.AddRange(
-                new Customer
-                {
-                    Fname = "Volodya",
-                    Lname ="Myk",
-                    Address = "Ivano-Frankivsk",
-                    Discount = 0.3
-                },
-                new Customer
-                {
-                    Fname = "Hlib",
-                    Lname = "Bond",
-                    Address = "Dnipro",
-                    Discount = 0.5
-                },
-                new Customer
-                {
-                    Fname = "Maksym",
-                    Lname = "Seer",
-                    Address = "Kyiv",
-                    Discount = 0.4
-                }
-            );
-            context.Supermarkets.AddRange(
-                new Supermarket
-                {
-                    Name = "Atb",
-                    Address = "Mazepy str"
-                },
-                new Supermarket
-                {
-                    Name = "Silpo",
-                    Address = "Shevchenka str"
-                },
-                new Supermarket
-                {
-                    Name = "Comfy",
-                    Address = "Parkova str"
-                }
-            );
-            context.Orders.AddRange(
-            
-                new Order
-                {
-                    Customer_Id = 1,
-                    Supermarket_Id = 1,
-                    Order_Date = new DateTime(2023,03,04),
-                    OrderDetails= new List<OrderDetails>
-                    {
-                        new OrderDetails
-                        {
-                            Product_Id = 1, 
-                            Quantity = 5
-                        },
-                    }
-                },
-                new Order
-                {
-                    Customer_Id = 2,
-                    Supermarket_Id = 2,
-                    Order_Date = new DateTime(2023,05,27),
-                    OrderDetails= new List<OrderDetails>
-                    {
-                        new OrderDetails
-                        {
-                            Product_Id = 3, 
-                            Quantity = 4
-                        },
-                    }
-                },
-                new Order
-                {
-                    Customer_Id = 3,
-                    Supermarket_Id = 3,
-                    Order_Date = new DateTime(2023,01,02),
-                    OrderDetails= new List<OrderDetails>
-                    {
-                        new OrderDetails
-                        {
-                            Product_Id = 2, 
-                            Quantity = 1
-                        },
-                    }
-                }
-            );
+
+            var products = new Product[]
+            {
+                new Product { Name = "Butter", Price = 30.0f },
+                new Product { Name = "Apple", Price = 20.50f },
+                new Product { Name = "Morshinska", Price = 9.30f }
+            };
+            context.Products.AddRange(products);
             context.SaveChanges();
 
+            var customers = new Customer[]
+            {
+                new Customer { Fname = "Volodya", Lname = "Myk", Address = "Ivano-Frankivsk", Discount = 0.3 },
+                new Customer { Fname = "Hlib", Lname = "Bond", Address = "Dnipro", Discount = 0.5 },
+                new Customer { Fname = "Maksym", Lname = "Seer", Address = "Kyiv", Discount = 0.4 }
+            };
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            var supermarkets = new Supermarket[]
+            {
+                new Supermarket { Name = "Atb", Address = "Mazepy str" },
+                new Supermarket { Name = "Silpo", Address = "Shevchenka str" },
+                new Supermarket { Name = "Comfy", Address = "Parkova str" }
+            };
+            context.Supermarkets.AddRange(supermarkets);
+            context.SaveChanges();
+
+            var orders = new Order[]
+            {
+                new Order { CustomerId = 1, SupermarketId = 1, OrderDate = new DateTime(2023, 3, 4) },
+                new Order { CustomerId = 2, SupermarketId = 2, OrderDate = new DateTime(2023, 5, 27) },
+                new Order { CustomerId = 3, SupermarketId = 3, OrderDate = new DateTime(2023, 1, 2) }
+            };
+            context.Orders.AddRange(orders);
+            context.SaveChanges();
+
+            var orderDetails = new OrderDetails[]
+            {
+                new OrderDetails { OrderId = 1, ProductId = 1, Quantity = 5 },
+                new OrderDetails { OrderId = 2, ProductId = 3, Quantity = 4 },
+                new OrderDetails { OrderId = 3, ProductId = 2, Quantity = 1 }
+            };
+            context.OrderDetails.AddRange(orderDetails);
+            context.SaveChanges();
         }
     }
 }
--- Sprint16/Models/Order.cs
+++ Sprint16/Models/Order.cs
@@ -5,12 +5,12 @@ namespace Sprint16.Models
     public class Order
     {
         public int Id { get; set; }
-        public int Customer_Id { get; set; }
-        public int Supermarket_Id { get; set; }
+        public int CustomerId { get; set; }
+        public int SupermarketId { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
-        public DateTime Order_Date { get; set;}
+        public DateTime OrderDate { get; set;}
 
         public Customer Customers { get; set; }
         public Supermarket Supermarkets { get; set; }
