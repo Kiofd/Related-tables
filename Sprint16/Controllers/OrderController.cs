8ed0894a46e1cc2aac80cfb426d855d060cff9be
Maksym <max.sheremeta228@gmail.com>
2023-12-11 20:04:45 +0200

    Create controllers

---
+++ Sprint16/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sprint16.Data;
+using Microsoft.EntityFrameworkCore;
+using Sprint16.Models;
+
+namespace Sprint16.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ShoppingContext _context;
+        public OrderController(ShoppingContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Orders.ToListAsync());
+        }
+    }
+}
---
+++ Sprint16/Controllers/SupermarketController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sprint16.Data;
+using Microsoft.EntityFrameworkCore;
+using Sprint16.Models;
+
+
+namespace Sprint16.Controllers
+{
+    public class SupermarketController : Controller
+    {
+        private readonly ShoppingContext _context;
+        public SupermarketController(ShoppingContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Supermarkets.ToListAsync());
+        }
+        public async Task<IActionResult> Create(Supermarket supermarket)
+        {
+            return View(supermarket);
+        }
+    }
+}
